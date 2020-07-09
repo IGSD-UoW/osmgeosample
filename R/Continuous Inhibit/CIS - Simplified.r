ab689e60a20f30f08f74466658247cc5636f7e97
Henry Crosby <67096487+CrosbyPhdData@users.noreply.github.com>
2020-07-09 14:26:32 +0100

    Update CIS - Simplified.r

--- R/Continuous Inhibit/CIS - Simplified.r
+++ R/Continuous Inhibit/CIS - Simplified.r
@@ -1,3 +1,26 @@
+###########Preamble ###############################
+
+#read in the correct libraries and data
+library("geoR")
+library(sp)
+library(sf)
+library(splancs)
+
+data("parana")
+
+# Define the parameters
+poly <- st_sf(st_sfc(st_polygon(
+  list(as.matrix(matrix(c(parana$borders[,1],parana$borders[,2]),dim(parana$borders)[1],2,byrow=FALSE)))))) # create a polygon from the matrix
+size =100 ## to number of locatoins to be sampled
+delta = 0.3 ## minimum permissable distance between any two locations in a sinly sample. 
+delta.fix = FALSE ## is the value fixed or does it vary with the number of close pairs.
+k=0 # number of locations in sample to be replaced by near neighbours of other sample locations to form close pair (max int size/2). k=0 is a simple inhibitory design.
+rho = NULL # the mex distance etween any two locations in a 'close pair'
+ntreis = 10000 # number of rejected proposals after which the algorithm will terminate.
+plotit = TRUE # determines whether we should plot the sample.
+
+
+## Function which draws a spatially continous sample of locations within a polygonal sampling region according to an "inhibitory plus close pairs" specification.
 contin.inhibit.simplified <-function(poly,size,delta, delta.fix = FALSE,
                                     k=0,rho=NULL, ntries = 10000, plotit = TRUE) {
   
@@ -8,25 +31,18 @@ contin.inhibit.simplified <-function(poly,size,delta, delta.fix = FALSE,
   xy.sample <- matrix(csr(st.poly,1),1,2)
   delta <- delta * sqrt(size/(size - k))
   dsq  <- delta*delta
-  xy.sample<-rbind(xy.sample,xy.try)
+  for (i in 2:size) {
+    xy.try <- c(csr(st.poly,1))
+    xy.sample <- rbind(xy.sample, xy.try)
+  }
   k.origin <- k
-  size <- dim(unique(xy.sample))[1]
   reduction <- ((orig.size - size)/orig.size)
   take<-matrix(sample(1:size,2*k,replace=FALSE),k,2)
-  for (j in 1:k) {
-    take1<-take[j,1]; take2<-take[j,2]
-    xy1<-c(xy.sample[take1,])
-    angle<-2*pi*runif(1)
-    radius<-rho*sqrt(runif(1))
-    xy.sample[take2,]<-xy1+radius*c(cos(angle),sin(angle))
-  }
-xy.sample <- xy.sample %>%
+ xy.sample <- xy.sample %>%
   as.data.frame %>%
   sf::st_as_sf(coords = c(1,2))
 sample.locs <- sf::st_as_sf(xy.sample)
-if(class(sample.locs)[1] != class(poly.origin)[1]){
-  sample.locs <- sf::as_Spatial(xy.sample, "Spatial")
-  
+
   par(oma=c(5, 5, 5, 5.5), mar=c(5.5, 5.1, 4.1, 2.1), mgp=c(3, 1, 0), las=0)
   plot(st_geometry(xy.sample),pch=19,col=1,axes = TRUE,
        xlab="longitude",ylab="lattitude", font.main = 3,
@@ -43,5 +59,27 @@ res$delta = delta
 res$k <- k
 res$sample.locs = sample.locs
 
-return(res)
 }
+
+# Generate spatially regular sample
+set.seed(5871120)
+contin.inhibit.simplified(poly,size=100,delta=3, delta.fix = FALSE, k=0,rho=NULL, ntries = 100000, plotit = TRUE)
+
+
+# Generate spatially regular sample
+set.seed(5871121)
+xy.sample1 <- contin.inhibit.simplified(poly=poly,size = 100, delta = 30, plotit = TRUE)
+
+
+# Generate spatially regular sample with 10 close pairs
+set.seed(5871122)
+xy.sample2 <- contin.inhibit.simplified(poly,size = 100, delta = 30,
+                                    k = 5, rho = 15, plotit = TRUE)
+
+# Generate spatially regular sample with 10 close pairs
+set.seed(5871123)
+xy.sample3 <- contin.inhibit.simplified(poly,size = 100, delta = 30, delta.fix = TRUE,
+                                    k = 10, rho = 15, plotit = TRUE)
+
+
+
