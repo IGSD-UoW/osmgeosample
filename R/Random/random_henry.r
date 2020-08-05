5e99d295f2f72010fd8f1fb98e9117c784a51847
CrosbyPhData <henry@ai-owl.co.uk>
2020-08-05 11:51:53 +0100

    tidied up some merge issues.
    Improved the plotit and fixed some bugs.

--- R/Random/random_henry.r
+++ R/Random/random_henry.r
@@ -1,9 +1,13 @@
 #######################
 ###############
+<<<<<<< HEAD
 library(sp)
 library(sf)
 library(splancs)
 library(osmdata)
+=======
+
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
 
 # 1. Sampling from a discrete set of points.
 library("dplyr")
@@ -42,6 +46,7 @@ xy.sample <- random.sample(poly = poly,size = 100, type = "continuum", plotit = 
 library(sp)
 library(sf)
 library(splancs)
+<<<<<<< HEAD
 library(osmdata)
 library(rgdal)
 
@@ -59,6 +64,20 @@ value = "yes"
 if (boundary == 0) {
   dat <-  opq (poly@bbox) %>%
     add_osm_feature (key=key, value=value) %>%
+=======
+
+
+poly <- readOGR(dsn="C:/Users/Henry/Documents/University of Warwick/Boundaries" , layer="Boundary_Idikan",verbose=FALSE) ## here you can read in any shapefile
+poly <- st_sf(st_sfc(st_polygon(list(as.matrix(poly)))))
+
+boundary<- 0
+jointype <- 1
+
+
+if (boundary == 0) {
+  dat <-  opq (poly@bbox) %>%
+    add_osm_feature (key="building", value="yes") %>%
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
     osmdata_sf () ## Returns all buildings within the bounding box
 
   dat_tr_ex <-trim_osmdata (dat, poly) # Returns all buildings that are fully within the specified area
@@ -67,7 +86,11 @@ if (boundary == 0) {
 } else if (boundary == 1) {
 
   dat <-  opq (poly@bbox) %>%
+<<<<<<< HEAD
     add_osm_feature (key=key, value=value) %>%
+=======
+    add_osm_feature (key="building", value="yes") %>%
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
     osmdata_sf () ## Returns all buildings within the bounding box
 
   coords <- rbind(
@@ -91,7 +114,11 @@ if (boundary == 0) {
   countries_buff_5km <- st_buffer(countries_for_buff, 0.05)
 
   dat_buf <-  opq (st_bbox(countries_buff_5km)) %>%
+<<<<<<< HEAD
     add_osm_feature (key=key, value=value) %>%
+=======
+    add_osm_feature (key="building", value="yes") %>%
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
     osmdata_sf () ## Returns all buildings within the bounding box
 
   dat_tr_ex <-trim_osmdata (dat, countries_buff_5km) # Returns all buildings that are fully within the specified area
@@ -99,7 +126,11 @@ if (boundary == 0) {
   boundary <- countries_buff_5km
 }
 
+<<<<<<< HEAD
 if (jointype == 0){obj <-dat_tr$osm_polygons } else {obj<-dat_tr_ex$osm_polygons}
+=======
+if (jointype == 0){buildings <-dat_tr$osm_polygons } else {buildings<-dat_tr_ex$osm_polygons}
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
 
 
 random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
@@ -108,9 +139,15 @@ random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
   if (is.null(type)){
     stop("\n 'type' must be provided")
   }
+<<<<<<< HEAD
   if (type != "discrete" & type != "continuum"){
     stop("'type' must be either 'discrete' or 'continuum'")
   }
+=======
+  if (type != "discrete" & type != "continuum")
+    stop("'type' must be either 'discrete' or 'continuum'")
+
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
   if (type == "discrete"){
     obj.origin <- obj
     if (is.null(obj))
@@ -191,9 +228,12 @@ random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
       as.data.frame %>%
       sf::st_as_sf(coords = c(1,2))
     res <- xy.sample <- sf::st_as_sf(xy.sample)
+<<<<<<< HEAD
     if(class(poly.origin)[1] == "SpatialPolygonsDataFrame"){
       poly.origin<-st_as_sf(poly.origin)
     }
+=======
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
     if(class(xy.sample)[1] != class(poly.origin)[1]){
       res <- sf::as_Spatial(xy.sample, "Spatial")
     }
@@ -202,6 +242,7 @@ random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
   if(plotit==TRUE){
     par(oma=c(5, 5, 5, 5.5), mar=c(5.5, 5.1, 4.1, 2.1), mgp=c(3, 1, 0), las=0)
     if (type == "discrete"){
+<<<<<<< HEAD
       if (class(obj.origin)[1] == "sf"){
         plot(st_geometry(obj.origin), pch = 19, col = "yellow", axes = TRUE,
            xlab = "longitude", ylab = "lattitude", font.main = 3,
@@ -214,6 +255,17 @@ random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
              main = paste("Random sampling design,", size, "points", sep = " "))
         }
       plot(st_geometry(xy.sample),pch=19, cex = 0.25, col=1, add = TRUE)
+=======
+      plot(st_geometry(xy.sample), pch = 19, col = 1, axes = TRUE,
+           xlab = "longitude", ylab = "lattitude", font.main = 3,
+           cex.main = 1.2, col.main = "blue",
+           main = paste("Random sampling design,", size, "points", sep = " "))
+      if (class(obj.origin)[1] == "sf"){
+        plot(st_geometry(obj.origin),pch=19, cex = 0.25, col="black", add = TRUE)
+      }else{
+        plot(obj.origin,pch=19, cex = 0.25, col="black", add = TRUE)
+      }
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
     } else{
       plot(st_geometry(xy.sample),pch=19,col=1,axes = TRUE,
            xlab="longitude",ylab="lattitude", font.main = 3, cex.main = 1.2, col.main = "blue",
@@ -230,6 +282,10 @@ random.sample <- function(obj = NULL, poly = NULL, type, size, plotit = TRUE)
 
 
 
+<<<<<<< HEAD
 random.sample(obj = obj, poly = poly, type="discrete", size, plotit = TRUE)
+=======
+random.sample(obj = buildings, poly = buildings, type, size, plotit = TRUE)
+>>>>>>> e957ef81415f9c25b8afdfe20aec6701e2f445b6
 
 
